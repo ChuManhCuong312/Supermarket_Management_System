aeef451d62fb43266e087dbff28d09e0f086754e
Justroamming <148474299+Justroamming@users.noreply.github.com>
2025-10-18 21:44:19 +0700

    feat(order): thêm api Tạo mới bản ghi đơn hàng trong DB, thêm file SecurityConfig.java để test api trong postman

--- backend/src/main/java/com/supermarket/management/repository/OrderRepository.java
+++ backend/src/main/java/com/supermarket/management/repository/OrderRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Integer> {
     // Fetch all orders sorted by order date
     List<Order> findAllByOrderByOrderDateAsc(); // ascending order
     List<Order> findAllByOrderByOrderDateDesc(); // descending order
