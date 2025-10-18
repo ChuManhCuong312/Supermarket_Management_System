98a90571ae651a5341100b4b793208dd3e077613
Justroamming <148474299+Justroamming@users.noreply.github.com>
2025-10-18 21:44:19 +0700

    feat(order): thêm api lấy danh sách đơn hàng từ db, thêm thực thể order,thêm kết nối mysql ở application.yml

--- backend/src/main/java/com/supermarket/management/SuperMarketManagementSystemApplication.java
+++ backend/src/main/java/com/supermarket/management/SuperMarketManagementSystemApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SuperMarketManagementSystemApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(SuperMarketManagementSystemApplication.class, args);
     }
 }
-
