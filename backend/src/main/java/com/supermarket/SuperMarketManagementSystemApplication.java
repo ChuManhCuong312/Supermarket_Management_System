931886075d2a57f0601e16a996439864c878233f
codesomethinguseful <hdang4747@gmail.com>
2025-10-18 23:11:50 +0700

    fix files directories

--- backend/src/main/java/com/supermarket/SuperMarketManagementSystemApplication.java
+++ backend/src/main/java/com/supermarket/SuperMarketManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package com.supermarket.management;
+package com.supermarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SuperMarketManagementSystemApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(SuperMarketManagementSystemApplication.class, args);
     }
 }
-
