3b06139e755d01865c52dd3ff6dd926646efa17d
Justroamming <148474299+Justroamming@users.noreply.github.com>
2025-10-18 21:44:19 +0700

    feat(order): thêm api lấy danh sách đơn hàng từ db, thêm thực thể order,thêm kết nối mysql ở application.yml

---
+++ backend/src/main/java/com/supermarket/management/service/OrderService.java
@@ -0,0 +1,19 @@
+package com.supermarket.management.service;
+
+import com.supermarket.management.entity.Order;
+import com.supermarket.management.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+}
