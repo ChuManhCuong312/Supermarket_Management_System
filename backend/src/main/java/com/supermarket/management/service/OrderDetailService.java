82c415c32be7bc891524032f9ae75bcd59a2f3e3
Justroamming <148474299+Justroamming@users.noreply.github.com>
2025-10-19 23:49:51 +0700

    feat(order): thêm api cho phép sort danh sách chi tiết đơn theo Thành tiền

--- backend/src/main/java/com/supermarket/management/service/OrderDetailService.java
+++ backend/src/main/java/com/supermarket/management/service/OrderDetailService.java
@@ -18,4 +18,8 @@ public class OrderDetailService {
     public List<OrderDetail> getAllOrderDetailsSortedAsc() {return orderDetailRepository.findAllByOrderByOrderIdAsc();}
 
     public List<OrderDetail> getAllOrderDetailsSortedDesc() {return orderDetailRepository.findAllByOrderByOrderIdDesc();}
+
+    public List<OrderDetail> getAllOrderDetailsSortedByTotalPriceAsc() {return orderDetailRepository.findAllByOrderByTotalPriceAsc();}
+
+    public List<OrderDetail> getAllOrderDetailsSortedByTotalPriceDesc() {return orderDetailRepository.findAllByOrderByTotalPriceDesc();}
 }
