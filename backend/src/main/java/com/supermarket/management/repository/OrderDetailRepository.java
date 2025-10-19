c39953af773d070ef2a4d87b3813f347a11cd88d
Justroamming <148474299+Justroamming@users.noreply.github.com>
2025-10-19 20:55:58 +0700

    feat(orderdetail): thêm entity OrderDetail.java + thêm api để lấy danh sách chi tiết đơn từ DB

---
+++ backend/src/main/java/com/supermarket/management/repository/OrderDetailRepository.java
@@ -0,0 +1,9 @@
+package com.supermarket.management.repository;
+
+import com.supermarket.management.entity.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+}
