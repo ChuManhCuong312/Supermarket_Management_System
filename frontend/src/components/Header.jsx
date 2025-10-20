42d1b00785deea6e084b0c6fb5fbbe122cf4ccf6
Tuan <mait2082005@gmail.com>
2025-10-20 23:33:00 +0700

    add heaader to components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header style={{ padding: "12px 16px", borderBottom: "1px solid #eee" }}>
+      <h1 style={{ margin: 0, fontSize: 18 }}>Supermarket Management</h1>
+    </header>
+  );
+}
+
+export default Header;
