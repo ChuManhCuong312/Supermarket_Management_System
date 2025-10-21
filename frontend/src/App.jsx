46e94b3a8536e10eb39f32565d0ecaab25fb4101
N.Duong <dn20050719@gmail.com>
2025-10-21 14:58:57 +0700

    feat(customer): frontend for customer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CustomerList from "./modules/customer/CustomerList";
+import EmployeeList from "./modules/employee/EmployeeList";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Trang chính</h1>} />
+        <Route path="/customers" element={<CustomerList />} />
+        <Route path="/employees" element={<EmployeeList />} />
+      </Routes>
+    </Router>
+  );
+}
