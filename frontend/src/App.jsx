af3f69d0da8dd56bf96ad64b1f6390e3f9019b47
codesomethinguseful <hdang4747@gmail.com>
2025-10-21 23:05:20 +0700

    frontend: kết nối Import APIs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CustomerList from "./modules/customer/CustomerList";
 import EmployeeList from "./modules/employee/EmployeeList";
+import ImportList from "./modules/import/ImportList";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<h1>Trang chính</h1>} />
         <Route path="/customers" element={<CustomerList />} />
         <Route path="/employees" element={<EmployeeList />} />
+        <Route path="/imports" element={<ImportList />} />
       </Routes>
     </Router>
   );
