672b46682c8b0df82c6802ac128e2e5a83eaff0d
codesomethinguseful <hdang4747@gmail.com>
2025-10-23 22:35:57 +0700

    frontend: fix hiển thị cho import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CustomerList from "./modules/customer/CustomerList";
 import EmployeeList from "./modules/employee/EmployeeList";
 import OrderList from "./modules/order/OrderList";
+import ImportList from "./modules/import/ImportList";
 import OrderDetailList from "./modules/orderDetail/OrderDetailList";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 
 export default function App() {
   return (
@@ -17,8 +17,9 @@ export default function App() {
         <Route path="/employees" element={<EmployeeList />} />
         <Route path="/orders" element={<OrderList />} />
         <Route path="/orderdetails" element={<OrderDetailList />} />
+        <Route path="/imports" element={<ImportList />} />
       </Routes>
-       <ToastContainer position="top-right" autoClose={2000} />
+      <ToastContainer position="top-right" autoClose={2000} />
     </Router>
   );
 }
